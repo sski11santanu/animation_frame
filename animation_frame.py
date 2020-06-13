d40abf7d2205691b2a8c5104942482a749f1bcb1
studsantanu <65644599+studsantanu@users.noreply.github.com>
2020-06-13 22:01:16 +0530

    Update animation_frame.py

--- animation_frame.py
+++ animation_frame.py
@@ -3,11 +3,11 @@ import time
 
 # func is the name of the function which has to be animated.
 # delay is the time gap (in milliseconds) between any two successive recursion of the function func.
-# ...args object stores the arguments of func.
-def animationFrame(func, delay, ...args):
+# *args object stores the arguments of func.
+def animationFrame(func, delay, *args):
     time.sleep(delay / 1000)
     
-    func(args)
+    func(*args)
     
 # Display some info about this module when run without importing.
 if __name__ == "__main__":
